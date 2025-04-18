445cd6bc8c787e9141070d159e5873e5e32ce1f3
Wsalas651 <ws2022073896@virtual.upt.pe>
2025-04-17 23:59:18 -0500

    Revision

--- Bank/Bank.WebApi/obj/Debug/net8.0/Bank.WebApi.AssemblyInfo.cs
+++ Bank/Bank.WebApi/obj/Debug/net8.0/Bank.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Banco API para laboratorio de CPS")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dee809137b97f5b08bc52de4a5cb3b6a185f309")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e418ced5863adc454b0ed0de57cad9c47c4ed7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
