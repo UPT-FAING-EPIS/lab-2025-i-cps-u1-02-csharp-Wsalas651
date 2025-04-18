0fb5ca34c22454442e7a051dcd00e9eedd7f92ed
Wsalas651 <ws2022073896@virtual.upt.pe>
2025-04-18 00:21:44 -0500

    Revision 2.0

--- Bank/Bank.WebApi.Tests/obj/Debug/net8.0/Bank.WebApi.Tests.AssemblyInfo.cs
+++ Bank/Bank.WebApi.Tests/obj/Debug/net8.0/Bank.WebApi.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.WebApi.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e418ced5863adc454b0ed0de57cad9c47c4ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5778ec6205992c0bf83fe4b91efd83b53939a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.WebApi.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.WebApi.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
