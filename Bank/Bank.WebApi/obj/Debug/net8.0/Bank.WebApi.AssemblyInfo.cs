c92b76754361c74c6e5c3008c08b5c4208a79f3f
Wsalas651 <ws2022073896@virtual.upt.pe>
2025-04-19 12:24:43 -0500

    Update 3.0

--- Bank/Bank.WebApi/obj/Debug/net8.0/Bank.WebApi.AssemblyInfo.cs
+++ Bank/Bank.WebApi/obj/Debug/net8.0/Bank.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c82e4fd2b768deec70774260acfbc9bf55ad01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f050852470a01802260e03777a29790daa3b9d9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
